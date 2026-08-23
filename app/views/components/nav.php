e6a9f466cbfb42638b8c33ae1aed9b252affceaf
suudbardjo@gmail.com <suudbardjo@gmail.com>
2026-08-23 23:39:39 +0700

    Hapus kembali file mati yang terbawa dari riwayat remote

--- app/views/components/nav.php
+++
@@ -1,8 +0,0 @@
-<header></header>
-<body>
-    <div class="navbar">
-        <a class="" href="index.php">Home</a>
-        <a class="" href="index.php?act=kategori">Kategori</a>
-        <a class="" href="index.php?act=catatan">Catatan</a>
-    </div>
-</body>
